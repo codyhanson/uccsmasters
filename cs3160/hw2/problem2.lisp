afa79d65a878f9833e5486a38a1b92e925b1889c
Cody Hanson <codyhanson50@gmail.com>
2012-11-04 17:26:50 -0700

    got deep reverse to work

--- cs3160/hw2/problem2.lisp
+++ cs3160/hw2/problem2.lisp
@@ -6,3 +6,9 @@
     ((null (cdr x)) x)
     ((listp x) (append (topreverse (cdr x)) (list(car x))))))
 
+(defun deepreverse (x)
+  (cond
+    ((atom x) x)
+    ((null (cdr x)) x)
+    ((listp x) (append (deepreverse (cdr x)) (list(deepreverse (car x)))))))
+
