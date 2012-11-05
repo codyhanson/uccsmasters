cb4885b9ba215180904066cfad759a433918f61b
Cody Hanson <codyhanson50@gmail.com>
2012-11-04 18:51:48 -0700

    problem 2 reversing is working for top and deep, debugged and working for the test cases. need to pretty up the presentation

--- cs3160/hw2/problem2.lisp
+++ cs3160/hw2/problem2.lisp
@@ -3,12 +3,24 @@
 
 (defun topreverse (x)
   (cond
+    ((atom x) x)
     ((null (cdr x)) x)
     ((listp x) (append (topreverse (cdr x)) (list(car x))))))
 
 (defun deepreverse (x)
   (cond
     ((atom x) x)
-    ((null (cdr x)) x)
+    ((null (cdr x)) (list (deepreverse (car x))))
     ((listp x) (append (deepreverse (cdr x)) (list(deepreverse (car x)))))))
 
+(print (topreverse (list 'a 'b 'c 'd)))
+(print (deepreverse (list 'a 'b 'c 'd)))
+
+(print (topreverse (list (list 'a 'b) (list (list'c 'd (list 'x (list 'y 'z))) 'e) 'f 'g)))
+(print (deepreverse (list (list 'a 'b) (list (list'c 'd (list 'x (list 'y 'z))) 'e) 'f 'g)))
+
+(print (topreverse 'a))
+(print (deepreverse 'a))
+
+(print (topreverse (list)))
+(print (deepreverse (list)))
