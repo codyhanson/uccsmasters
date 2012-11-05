bf2226e3dcf234034f87b298cfad9a64aed75914
Cody Hanson <codyhanson50@gmail.com>
2012-11-04 19:46:13 -0700

    started working on the problem 3

---
+++ cs3160/hw2/problem3.lisp
@@ -0,0 +1,17 @@
+;Cody Hanson
+;CS 3160
+;UCCS Fall 2012
+;Problem 3 hw 2
+;11/4/2012
+
+
+(defun contains (x y)
+
+
+
+(defun atomsonly (x)
+
+
+;'main' loop to take in input for x and y
+(loop while 
+
