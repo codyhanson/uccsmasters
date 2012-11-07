064a05331d07aed73705ecd013b5661806084802
Cody Hanson <codyhanson50@gmail.com>
2012-11-06 20:14:08 -0700

    got atomsonly to work

--- cs3160/hw2/problem3.lisp
+++ cs3160/hw2/problem3.lisp
@@ -6,12 +6,33 @@
 
 
 (defun contains (x y)
+  (cond
+  ((equal (car x) (car y)) (contains (cdr x) (cdr y)))
+  (format t x)
+  (format t y)))
 
 
-
+;returns true if x is composed of only atoms
+;returns nil if there is a nested list
 (defun atomsonly (x)
+  (print x)
+  (if (null x) 
+    t 
+    (if (atom (car x))
+      (atomsonly (cdr x))
+      nil)))
+
+
 
 
 ;'main' loop to take in input for x and y
-(loop while 
+(format t "Please Input Y:")
+(setq y (read))
+;now get X and compute if X is in Y
+(format t "Please Input X:")
+(loop while (setq x (read)) do
+  ;(contains x y) 
+  (print (atomsonly x))
+  (format t "Please Input X:")
+  (terpri))
 
