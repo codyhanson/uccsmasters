7fdef022599d448b2b12b912d6fff61319c0edc0
Cody Hanson <codyhanson50@gmail.com>
2012-11-12 15:32:13 -0700

    workign on setval for p4

--- cs3160/hw2/problem4.lisp
+++ cs3160/hw2/problem4.lisp
@@ -1,30 +1,44 @@
 
 
+;if the current index is less than the index i want to insert into
+;go to the next item in the list
+;if the current index is greater than the index I want to insert into
+;insert a new node before this index.
+;if the current index id equal, then update this item.
+
 (defun setval (alist index val)
+  (print alist)
+  (print index)
+  (print val)
+  (cond 
+  ((= nil val) alist) ;nil value, return unmodified list
+  ((null alist)  (list (list index val)))
+  ((null (cdr alist)) (list (car alist) (list index val))) 
+  ((< index (car (car alist))) (setval (cdr alist) index val))
+  ((> index (car (car alist))) (list (car alist ) (list index val) (cdr alist)))
+  ((= index (car (car alist))) (list (list index val) (cdr alist)))))
 
-)
 
+;(defun getval (alist index)
 
-(defun getval (alist index)
+;)
 
-)
+;(defun display (mylist all)
 
-(defun display (mylist all)
+;)
 
-)
+;(defun compress (mylist)
 
-(defun compress (mylist)
+;)
 
-)
+;(defun size (mylist)
 
-(defun size (mylist)
+;)
 
-)
+;(defun swap (mylist x y)
 
-(defun swap (mylist x y)
+;)
 
-)
+;(defun bubblesort (mylist)
 
-(defun bubblesort (mylist)
-
-)
+;)
