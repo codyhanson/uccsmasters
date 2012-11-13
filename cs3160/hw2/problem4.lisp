1e54eeb8d247b032ce8586c92077284e0ddf3772
Cody Hanson <codyhanson50@gmail.com>
2012-11-12 18:18:45 -0700

    got progress on size, getval, setval

--- cs3160/hw2/problem4.lisp
+++ cs3160/hw2/problem4.lisp
@@ -7,33 +7,61 @@
 ;if the current index id equal, then update this item.
 
 (defun setval (alist index val)
-  (print alist)
-  (print index)
-  (print val)
   (cond 
-  ((= nil val) alist) ;nil value, return unmodified list
+  ((null val) alist) ;nil value, return unmodified list
   ((null alist)  (list (list index val)))
-  ((null (cdr alist)) (list (car alist) (list index val))) 
-  ((< index (car (car alist))) (setval (cdr alist) index val))
-  ((> index (car (car alist))) (list (car alist ) (list index val) (cdr alist)))
+  ((< index (car (car alist))) (cons (list index val) alist ))
+  ((> index (car (car alist))) (cons (car alist) (setval (cdr alist) index val)))
   ((= index (car (car alist))) (list (list index val) (cdr alist)))))
 
+(defun getval (alist index)
+  (cond
+  ((null alist) nil)
+  ((null index) nil)
+  ((= index (car (car alist))) (cdr (car alist)))
+  (T (getval (cdr alist) index))))
 
-;(defun getval (alist index)
+(defun size (mylist)
+  (cond
+  ((null mylist) nil)
+  ((null (cdr mylist)) (car (car mylist)))
+  (T (size (cdr mylist)))))
 
-;)
+
+(defun compress (mylist)
+  (cond 
+  ((null mylist) nil)
+  (T (cons (list (1) (cdr (car mylist))) (compress (cdr mylist))))))
+
 
 ;(defun display (mylist all)
+;  (cond
+;  ((null alist) nil)
+;  ((null index) nil)
+;  ((= index (car (car alist))) (cdr (car alist)))
+;  (T (display(cdr alist) index))))
 
-;)
+(setf x (setval nil 10 8))
+(print x)
+(setf x (setval x 5 6))
+(print x)
+(setf x (setval x 1 4))
+(print x)
+(setf x (setval x 2 10))
+(print x)
+(setf x (setval x 7 25))
+(print x)
+(setf x (setval x 8 nil))
+(print x)
 
-;(defun compress (mylist)
+(print (getval x 1))
+(print (getval x 3))
+(print (getval x 5))
 
-;)
+(print (size x))
 
-;(defun size (mylist)
 
-;)
+
 
 ;(defun swap (mylist x y)
 
