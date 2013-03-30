9a53108dc798bbd65f9848c00e3f71ea45669f66
Cody Hanson <codyhanson50@gmail.com>
2013-03-30 07:58:53 -0600

    edited the rough draft

--- appliedcrypto/project/build.sh
+++ appliedcrypto/project/build.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 latex hanson_proposal
+latex hanson_proposal
 bibtex hanson_proposal.aux
+latex hanson_proposal
 latex hanson_proposal
 latex hanson_proposal
 dvipdfm hanson_proposal.dvi
