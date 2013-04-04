30e1e8fb02914d7c09a495abb7566af9b5456c23
Cody Hanson <cody.hanson@flukenetworks.com>
2013-04-04 14:47:45 -0600

    factored out the block_size var

--- appliedcrypto/pycrypto/AESCipher.py
+++ appliedcrypto/pycrypto/AESCipher.py
@@ -3,7 +3,7 @@ from Crypto import Random
 import base64
 
 #blocksize and padding functions
-BS = 16
+BS = AES.block_size 
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
 unpad = lambda s : s[0:-ord(s[-1])]
 
@@ -14,14 +14,14 @@ class AESCipher:
 
     def encrypt( self, raw ):
         raw = pad(raw)
-        iv = Random.new().read( AES.block_size )
+        iv = Random.new().read( BS )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
         return base64.b64encode( iv + cipher.encrypt( raw ) ) 
 
     def decrypt( self, enc ):
         enc = base64.b64decode(enc)
-        iv = enc[:16]
+        iv = enc[:BS]
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return unpad(cipher.decrypt( enc[16:] ))
+        return unpad(cipher.decrypt( enc[BS:] ))
 
 
