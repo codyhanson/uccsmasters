4c4f7f19257a138ab08f550881b208afaee936bb
Cody Hanson <cody.hanson@flukenetworks.com>
2014-02-23 21:34:28 -0700

    Got the initial substitution working

--- ai/hw1/rules_engine.lisp
+++ ai/hw1/rules_engine.lisp
@@ -18,17 +18,41 @@
 
 (defun sub (input subs)
    ;check the first token to decide how to recurse
+   ;(print '------)
+   ;(print subs)
+   ;(print input)
+   ;(print (car input))
+   ;(print (cdr input))
    (cond
+     ;if no input, nothing to do.
+     ((null input) NIL)
      ;if it is a simple word, just keep going.
      ((atom (car input)) (cons (car input) (sub (cdr input) subs)))
      ;if it is a nested list, and also a replacement marker (? something), do replacement
-     ((and (listp (car input)) (equal (car input) '?)) 
-         (loop for sub in subs do
-             (if (equal (car sub) (nth 1 input)) sub)
+     ((and (listp (car input)) (equal (car (car input)) '?)) 
+         (loop for pair in subs do
+            ; (print '-loop-)
+            ; (print pair )
+            ; (print (car pair ))
+            ; (print (nth 1 (car input)))
+             (if (equal (car pair) (nth 1 (car input))) 
+                 (return (cons (nth 1 pair)  (sub (cdr input) subs)))
+             )
          ))
      ;if it is just a nested list, recurse on both the first element, and rest of the list.
      ((listp (car input)) (cons (sub (car input) subs) (sub (cdr input) subs)))
     ))
+
+
+(setf result (sub '(color (? x) (? y)) '((x apple) (y red))))
+(print '---------)
+(print 'result)
+(print result)
+
+(setf result (sub '(color ((? y) so very (? x)) word (? y)) '((x apple) (y red))))
+(print '---------)
+(print 'result)
+(print result)
 
 
 ;(defun applyRule (tree rule)
