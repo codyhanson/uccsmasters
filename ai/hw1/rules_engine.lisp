7b92d30feab9168896b90635c1aa75da09da84f0
Cody Hanson <cody.hanson@flukenetworks.com>
2014-02-25 22:01:44 -0700

    made some progress on the match function

--- ai/hw1/rules_engine.lisp
+++ ai/hw1/rules_engine.lisp
@@ -26,6 +26,8 @@
    (cond
      ;if no input, nothing to do.
      ((null input) NIL)
+     ;if no subs, just return the input
+     ((null subs) input)
      ;if it is a simple word, just keep going.
      ((atom (car input)) (cons (car input) (sub (cdr input) subs)))
      ;if it is a nested list, and also a replacement marker (? something), do replacement
@@ -43,27 +45,56 @@
      ((listp (car input)) (cons (sub (car input) subs) (sub (cdr input) subs)))
     ))
 
+(defun applyRule (tree rule)
+    ;matches return the applied rule
+  (sub (match tree (lhs rule)) (rhs rule))) 
 
-(setf result (sub '(color (? x) (? y)) '((x apple) (y red))))
+(defun match (tree lhsrule)
+  (let (associations null) 
+    (setf pairs (mapcar #'list tree lhsrule))
+    (print pairs)
+    (loop for pair in pairs do 
+     (print '-----1)
+     (print associations)
+     (print pair) 
+     (cond 
+      ;if the pairs of atoms are equal, then keep going.
+      ((and (atom (nth 0 pair)) (atom (nth 1 pair))) (print 'keepgoing))
+
+      ;if the first is an atom, and the next is a substitution,
+      ;like (? x),add to the subs
+      ((and (atom (nth 0 pair)) 
+          (listp (nth 1 pair)) 
+          (eq (nth 0 (nth 1 pair)) '?)
+          (atom (nth 1 (nth 1 pair)))) (setf associations (cons pair associations)))
+      ;if not, abort, they don't match. return an empty subs list
+      (T (return-from match null))
+      ))
+    (print associations)
+    (return-from match associations)
+  )
+)
+
+
+;Main program.
+
+;(setf result (sub '(color (? x) (? y)) '((x apple) (y red))))
+;(print '---------)
+;(print 'result)
+;(print result)
+
+;(setf result (sub '(color ((? y) so very (? x)) word (? y)) '((x apple) (y red))))
+;(print '---------)
+;(print 'result)
+;(print result)
+
+;(setf result (match '(socrates man) '((? x) mortal)))
+;(print '---------)
+;(print 'result)
+;(print result)
+
+(setf result (match '(socrates is a man) '((? x) mortal)))
 (print '---------)
 (print 'result)
 (print result)
 
-(setf result (sub '(color ((? y) so very (? x)) word (? y)) '((x apple) (y red))))
-(print '---------)
-(print 'result)
-(print result)
-
-
-;(defun applyRule (tree rule)
-;  (cond 
-;    ;matches return the applied rule
-;    ((matches tree (lhs rule)) (sub (rhs rule) tree) 
-;    ;otherwise just return the tree
-;    (T tree) 
-;  )
-;)
-
-;(defun matches ()
-
-;  )
