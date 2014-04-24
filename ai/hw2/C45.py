9c2c20501c0745cbd5d897a8a4d552c1978a5eb8
Cody Hanson <cody.hanson@flukenetworks.com>
2014-04-23 22:47:15 -0600

    Got a tree building, continuous values. no missing. need to implement missing still, and verify that the tree building is happening approprately.

--- ai/hw2/C45.py
+++ ai/hw2/C45.py
@@ -3,6 +3,8 @@
 #This module implements the C4.5 Algorithm for a decision tree classifier
 from collections import defaultdict
 import math
+import re
+from numpy import median
 
 MINIMUM_SET_SIZE = 5
 
@@ -11,12 +13,16 @@ Tree = lambda: defaultdict(Tree)
 
 
 #recursive function to induce a decision tree from a set of training case data.
-def induce_tree(s):
+def induce_tree(schema, s):
     #first check for base cases
-    if are_in_same_class(s):
+    if len(schema) is 0:
+        return 'END'#todo what should happen here?
+    elif are_in_same_class(s):
         #base case! return a leaf node.
+        print "All same class base case for {0} {1}".format(schema, s)
         return get_class(s[0])
     elif set_is_small(s):
+        print "Small set base case for {0} {1}".format(schema, s)
         #set of cases is below the minimum size, take the most common class as a leaf node
         return most_common_class(s)
     else:
@@ -24,10 +30,12 @@ def induce_tree(s):
         #and split on the node that has the most information gain
         gains = find_information_gains(s)
         split_node_index = max_gain_index(gains)
+        print "Splitting on attr {0} index:{1}".format(schema[split_node_index], split_node_index)
         tree = Tree()
         partitions = partition_on_attribute(split_node_index, s)
         for key in partitions.keys():
-            tree['decision?'][key] = induce_tree(strip_partition_by_index(split_node_index, partitions[key]))
+            tree[schema[split_node_index].upper()][key] = \
+                induce_tree(strip_schema_by_index(split_node_index, schema), strip_partition_by_index(split_node_index, partitions[key]))
         return tree
 
 
@@ -41,11 +49,17 @@ def strip_partition_by_index(index,s):
     return stripped
 
 
+def strip_schema_by_index(index, schema):
+    tmp = list(schema)
+    del tmp[index]
+    return tuple(tmp)
+
+
 def find_information_gains(s):
     num_attributes = len(s[0]) - 1 #don't want to include the class
     gains = []
     for i in range(0, num_attributes):
-        probabilities = compute_probabilities(i,s)
+        probabilities = compute_probabilities(i, s)
         gains.append(information_gain(probabilities))
     return gains
 
@@ -58,17 +72,56 @@ def information_gain(probabilities):
     return ig
 
 
+def attribute_is_continuous(index, cases):
+    continuous = True
+    continuous_pattern = re.compile('[\d\.]+')
+    for case in cases:
+        if len(case) < index:
+            pass
+        m = re.match(continuous_pattern, case[index])
+        if m is None:
+            continuous = False
+            break
+    return continuous
+
+
+#this function takes a list of training cases,
+#and for each continuous attribute, discretizes it into ranges.
+#this then returns the processed set.
+#this function should be ran before the training set is passed into induce_tree
+def preprocess_continuous_attrs(schema, cases):
+    print 'Preprocessing data to get rid of continuous attributes...'
+    #iterate over each attribute.
+    modified_cases = cases
+    for i in range(0, len(modified_cases[0]) - 1):
+        print 'Processing {0}'.format(schema[i])
+        if not attribute_is_continuous(i, modified_cases):
+            #don't need to process this attribute
+            continue
+
+        values = [float(case[i]) for case in modified_cases]
+        med_val = median(values)
+        for j in range(0, len(modified_cases)):
+            tmp = list(modified_cases[j])
+            number_to_compare = float(modified_cases[j][i])
+            if (number_to_compare <= med_val):
+                tmp[i] = 'low'
+            else:
+                tmp[i] = 'high'
+            modified_cases[j] = tuple(tmp)
+    return modified_cases
+
 def compute_probabilities(index, cases):
     probabilities = []
     total = 0.0
     attr_values = defaultdict(int)
+
     for case in cases:
         total += 1.0
         attr_values[case[index]] += 1
     for attr_name in attr_values.keys():
         probabilities.append(attr_values[attr_name]/total)
     return probabilities
-
 
 
 #returns the index of the highest gain value
--- ai/hw2/run.py
+++ ai/hw2/run.py
@@ -2,24 +2,29 @@
 
 #python libs
 import sys
-
+import json
 #my libs
 import C45
 import classify
 
 #command line args
-training_data_file = sys.argv[1]
-#test_data_file = sys.argv[2]
+schema_file = sys.argv[1]
+training_data_file = sys.argv[2]
+#test_data_file = sys.argv[3]
 
+schema = tuple([raw_line.strip().split(', ') for raw_line in open(schema_file, 'r')][0])
 #read in the dataset, stripping newlines from the end. split each record on a comma, into a tuple.
 training_tuples = []
-for line in [raw_line.strip().rstrip() for raw_line in open(training_data_file, 'r')]:
-    training_tuples.append(tuple(line.split(',')))
+for line in [raw_line.strip() for raw_line in open(training_data_file, 'r')]:
+    training_tuples.append(tuple(line.split(', ')))
 
 
+processed_training_tuples = C45.preprocess_continuous_attrs(schema, training_tuples)
 
-tree = C45.induce_tree(training_tuples)
+tree = C45.induce_tree(schema, processed_training_tuples)
+print json.dumps(tree, indent=1)
 pruned_tree = C45.prune(tree)
-pass
+
+#then we can format the pruned tree as a list of rules.
 
 #classification = classify.classify(tree,example)
